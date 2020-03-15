45a56ab3d657aa810b090b559863190367f9437f
hqchqc <734986933@qq.com>
2020-03-15 17:24:15 +0800

    banner

---
+++ src/components/common/swiper/index.js
@@ -0,0 +1,7 @@
+import Swiper from './Swiper.vue'
+import SwiperItem from './SwiperItem.vue'
+
+export {
+    Swiper,
+    SwiperItem
+}
--- src/views/Find/childFinds/FindSwiper.vue
+++ src/views/Find/childFinds/FindSwiper.vue
@@ -1,14 +1,12 @@
 <template>
     <div>
-        <p>我是发现</p>
+        
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Find',
-    components:{
-    }
+
 }
 </script>
 
