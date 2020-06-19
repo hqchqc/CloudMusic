ff10b61fdbcfefce9bfd334b7aaec8718ee50ecf
hqchqc <734986933@qq.com>
2020-06-19 21:06:23 +0800

    s

--- src/views/SingerClass/SingerClass.vue
+++ src/views/SingerClass/SingerClass.vue
@@ -12,7 +12,8 @@
             <router-link to="/singerClass/other">其它</router-link>
             <router-link to="/singerClass/other">其它</router-link> -->
             <!-- <router-link to="/singerClass/translate/male">男</router-link> -->
-<a>男</a>
+            <!-- <router-link to="/singerClass/translate/male">男</router-link> -->
+            <a>男</a>
             <a>女</a>
             <a>男</a>
             <a>女</a>
